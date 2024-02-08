99a922aceabac788b3e7a0fbe19d92113f6a9580
Alex Hewitt <alexhewitt.j@gmail.com>
2024-02-08 07:59:50 -0600

    add useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -8,69 +8,78 @@ import MovieList from './components/MovieList'
 import WatchedSummary from './components/WatchedSummary'
 import WatchedMovieList from './components/WatchedMovieList'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 const tempMovieData = [
-    {
-        imdbID: 'tt1375666',
-        Title: 'Inception',
-        Year: '2010',
-        Poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg'
-    },
-    {
-        imdbID: 'tt0133093',
-        Title: 'The Matrix',
-        Year: '1999',
-        Poster: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg'
-    },
-    {
-        imdbID: 'tt6751668',
-        Title: 'Parasite',
-        Year: '2019',
-        Poster: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg'
-    }
+	{
+		imdbID: 'tt1375666',
+		Title: 'Inception',
+		Year: '2010',
+		Poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg'
+	},
+	{
+		imdbID: 'tt0133093',
+		Title: 'The Matrix',
+		Year: '1999',
+		Poster: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg'
+	},
+	{
+		imdbID: 'tt6751668',
+		Title: 'Parasite',
+		Year: '2019',
+		Poster: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg'
+	}
 ]
 const tempWatchedData = [
-    {
-        imdbID: 'tt1375666',
-        Title: 'Inception',
-        Year: '2010',
-        Poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-        runtime: 148,
-        imdbRating: 8.8,
-        userRating: 10
-    },
-    {
-        imdbID: 'tt0088763',
-        Title: 'Back to the Future',
-        Year: '1985',
-        Poster: 'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-        runtime: 116,
-        imdbRating: 8.5,
-        userRating: 9
-    }
+	{
+		imdbID: 'tt1375666',
+		Title: 'Inception',
+		Year: '2010',
+		Poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+		runtime: 148,
+		imdbRating: 8.8,
+		userRating: 10
+	},
+	{
+		imdbID: 'tt0088763',
+		Title: 'Back to the Future',
+		Year: '1985',
+		Poster: 'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+		runtime: 116,
+		imdbRating: 8.5,
+		userRating: 9
+	}
 ]
+const API_KEY = 'a5ceefdf'
 function App() {
-    const [movies, setMovies] = useState(tempMovieData)
-    const [watched, setWatched] = useState(tempWatchedData)
-    return (
-        <>
-            <NavBar>
-                <Logo />
-                <Search />
-                <NumResults movies={movies} />
-            </NavBar>
+	const [movies, setMovies] = useState(tempMovieData)
+	const [watched, setWatched] = useState(tempWatchedData)
 
-            <Main>
-                <Box>
-                    <MovieList movies={movies} />
-                </Box>
-                <Box>
-                    <WatchedSummary watched={watched} />
-                    <WatchedMovieList watched={watched} />
-                </Box>
-            </Main>
-        </>
-    )
+	useEffect(function () {
+		fetch(
+			`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=interstellar`
+		)
+			.then((res) => res.json())
+			.then((data) => setMovies(data.Search))
+	}, [])
+	return (
+		<>
+			<NavBar>
+				<Logo />
+				<Search />
+				<NumResults movies={movies} />
+			</NavBar>
+
+			<Main>
+				<Box>
+					<MovieList movies={movies} />
+				</Box>
+				<Box>
+					<WatchedSummary watched={watched} />
+					<WatchedMovieList watched={watched} />
+				</Box>
+			</Main>
+		</>
+	)
 }
 
 export default App
